a0383ce9a7a4e510f528a880bf6991e14492e38a
Sebastian Kumór <thekumor@gmail.com>
2025-07-31 11:34:09 +0200

    Added SDL
    + Added SDL3 as a submodule

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,11 @@
 mkdir build
+
+#Directories
 Copy-Item src/ -Destination build/ -Recurse
+Copy-Item vendor/ -Destination build/ -Recurse
+
+# Files
+Copy-Item vendor/SDL-3.2.18/lib-win32-x64/SDL3.dll -Destination build/
+
 cd build
 cmake ..
